f1dc9ea9d95f900588b369de06923290275ed775
unknown <ivan.nikic@gmail.com>
2020-09-09 01:41:44 +0200

    Updates

---
+++ precache-manifest.1a4940604e9e236ccfd49d9adec4e5e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68ba1b26cc77ff4ee25b02d1987efe86",
+    "url": "/spotify-clone-app/index.html"
+  },
+  {
+    "revision": "bfd22ee4af9122db53dd",
+    "url": "/spotify-clone-app/static/css/main.bc8376cb.chunk.css"
+  },
+  {
+    "revision": "045d769e265212d51c2b",
+    "url": "/spotify-clone-app/static/js/2.1e329ea7.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/spotify-clone-app/static/js/2.1e329ea7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfd22ee4af9122db53dd",
+    "url": "/spotify-clone-app/static/js/main.77e68028.chunk.js"
+  },
+  {
+    "revision": "c728961e951958ebd672",
+    "url": "/spotify-clone-app/static/js/runtime-main.79a129fa.js"
+  }
+]);
