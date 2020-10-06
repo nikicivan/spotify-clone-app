2fda957375a6cc2347eb80b90bcb33803a1da3a5
unknown <ivan.nikic@gmail.com>
2020-10-06 11:34:48 +0200

    added favicon

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,25 +1,24 @@
-import React from 'react';
-import './header.css';
-import SearchIcon from '@material-ui/icons/Search';
-import { Avatar } from '@material-ui/core';
-import { useStateProviderValue } from '../../contextAPI/stateProvider';
+import React from "react";
+import "./header.css";
+import SearchIcon from "@material-ui/icons/Search";
+import { Avatar } from "@material-ui/core";
+import { useStateProviderValue } from "../../contextAPI/stateProvider";
 
 const Header = () => {
+  const [{ user }] = useStateProviderValue();
 
-    const [{user}] = useStateProviderValue();
-
-    return (
-        <div className="header">
-            <div className="header-left">
-                <SearchIcon />
-                <input placeholder="search for Artists or Songs" type="text"/>
-            </div>
-            <div className="header-right">
-                <Avatar src={user?.images[0]?.url} alt={user?.display_name}/>
-                <h4>{user?.display_name}</h4>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="header">
+      <div className="header-left">
+        <SearchIcon />
+        <input placeholder="search for Artists or Songs" type="text" />
+      </div>
+      <div className="header-right">
+        <Avatar src={user?.images[0]?.url} alt={user?.display_name} />
+        <h4>{user?.display_name}</h4>
+      </div>
+    </div>
+  );
+};
 
 export default Header;
